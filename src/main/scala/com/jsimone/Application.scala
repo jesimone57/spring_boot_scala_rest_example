18c1539b61f2d1d3a350dbcb6055889a4fe42e3c
jesimone57 <jesimone57@yahoo.com>
2017-12-14 13:33:50 -0500

    cleanup

--- src/main/scala/com/jsimone/Application.scala
+++ src/main/scala/com/jsimone/Application.scala
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 /**
-  * This is the Spring boot entrypoint class.
-  * At runtime, this class boots the Spring application and starts the spring context.
-  * The Object keyword is used to create a singleton instance of the class in Scala.
+  * This is the Spring Boot entry-point class.
+  * At runtime, this class boots the Spring application and starts the Spring context.
+  * The object keyword is used to create a singleton instance of the class in Scala.
   */
 @SpringBootApplication
 class Application
