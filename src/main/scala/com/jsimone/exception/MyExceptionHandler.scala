58c0c5061c3af317b81c34332ec11db11ec52bc4
jesimone57 <jesimone57@yahoo.com>
2018-02-16 12:55:12 -0500

    remove unecessary code

--- src/main/scala/com/jsimone/exception/MyExceptionHandler.scala
+++ src/main/scala/com/jsimone/exception/MyExceptionHandler.scala
@@ -9,8 +9,8 @@ import org.springframework.beans.TypeMismatchException
 import org.springframework.http._
 import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.validation.BindException
+import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.{MethodArgumentNotValidException, MissingPathVariableException, MissingServletRequestParameterException}
-import org.springframework.web.bind.annotation.{ExceptionHandler, ResponseStatus}
 
 /**
   * Spring MVC Exceptions see https://docs.spring.io/spring/docs/3.2.x/spring-framework-reference/html/mvc.html#mvc-ann-rest-spring-mvc-exceptions
@@ -32,56 +32,48 @@ import org.springframework.web.bind.annotation.{ExceptionHandler, ResponseStatus
 class MyExceptionHandler extends Logging {
 
   @ExceptionHandler(Array(classOf[BindException]))
-  @ResponseStatus(HttpStatus.BAD_REQUEST)
   protected def handleBindException(exception: BindException, request: HttpServletRequest): ResponseEntity[AnyRef] = {
     log.error(exception.toString)
     buildBadRequestResponse(exception, request)
   }
 
   @ExceptionHandler(Array(classOf[MissingServletRequestParameterException]))
-  @ResponseStatus(HttpStatus.BAD_REQUEST)
   def handleMissingServletRequestParameterException(exception: MissingServletRequestParameterException, request: HttpServletRequest): ResponseEntity[AnyRef] = {
     log.error(exception.toString)
     buildBadRequestResponse(exception, request)
   }
 
   @ExceptionHandler(Array(classOf[TypeMismatchException]))
-  @ResponseStatus(HttpStatus.BAD_REQUEST)
   def handleTypeMismatchException(exception: TypeMismatchException, request: HttpServletRequest): ResponseEntity[AnyRef] = {
     log.error(exception.toString)
     buildBadRequestResponse(exception, request)
   }
 
   @ExceptionHandler(Array(classOf[MethodArgumentNotValidException]))
-  @ResponseStatus(HttpStatus.BAD_REQUEST)
   def handleMethodArgumentNotValidException(exception: MethodArgumentNotValidException, request: HttpServletRequest): ResponseEntity[AnyRef] = {
     log.error(exception.toString)
     buildBadRequestResponse(exception, request)
   }
 
   @ExceptionHandler(Array(classOf[HttpMessageNotReadableException]))
-  @ResponseStatus(HttpStatus.BAD_REQUEST)
   def handleHttpMessageNotReadableException(exception: HttpMessageNotReadableException, request: HttpServletRequest): ResponseEntity[AnyRef] = {
     log.error(exception.toString)
     buildBadRequestResponse(exception, request)
   }
 
   @ExceptionHandler(Array(classOf[JsonProcessingException]))
-  @ResponseStatus(HttpStatus.BAD_REQUEST)
   def handleJsonProcessingException(exception: JsonProcessingException, request: HttpServletRequest): ResponseEntity[AnyRef] = {
     log.error(exception.toString)
     buildBadRequestResponse(exception, request)
   }
 
   @ExceptionHandler(Array(classOf[MissingPathVariableException]))
-  @ResponseStatus(HttpStatus.BAD_REQUEST)
   def handleMissingPathVariableException(exception: MissingPathVariableException, request: HttpServletRequest): ResponseEntity[AnyRef] = {
     log.error(exception.toString)
     buildBadRequestResponse(exception, request)
   }
 
   @ExceptionHandler(Array(classOf[ErrorResponseException]))
-  @ResponseStatus(HttpStatus.BAD_REQUEST)
   def handleErrorResponseException(exception: ErrorResponseException, request: HttpServletRequest): ResponseEntity[AnyRef] = {
     log.error(exception.toString)
     buildBadRequestResponse(exception, request)
